70e999f816c2cb6bc7b31df0a4aa5938715d57a5
Lucian Boatright <lucian.boatright@gmail.com>
2020-07-15 15:01:51 +0100

    Twilio set up alterations

--- server/index.js
+++ server/index.js
@@ -2,16 +2,14 @@ require('dotenv').config();
 console.log(process.env);
 // dependencies
 // yarn add express cors wtilio
-
-const express = require('express')
-const cors = require('cors')
-const twilio = require('twilio')
+const express = require('express');
+const cors = require('cors');
+const twilio = require('twilio');
 
 // twilio rewuierments texting api
-
-const accountSid = process.env.ACCOUNT_SID // TWILIO SID NUM 
-const authToken = process.env.AUTH_TOKEN// TWILIO AUTH TOKEN
-const clinet = require('twilio')(accountSid, authToken) //new twilio(accountSid, authToken)
+const accountSid = process.env.ACCOUNT_SID; // TWILIO SID NUM 
+const authToken = process.env.AUTH_TOKEN;// TWILIO AUTH TOKEN
+const client = new twilio(accountSid, authToken); //require('twilio')(accountSid, authToken)
 
 
 const app = express();  //alias
@@ -21,20 +19,18 @@ app.use(cors());  //blocks browser from resticting data
 
 
 //welcome page server
-
-
 app.get('/', (reg, res) => {
-    res.send("WElcome from the server");
+    res.send("WElcome to the express server");
 })
 
 // twilio text
-
 app.get('/send-text', (reg, res) => {
     // GET variables via query string
+    res.send("Hello to the twilio sever")
     const { recipient, textmessage } = reg.query
 
 
-    clinet.messages.create({
+    client.messages.create({
         body: textmessage,
         to: recipient,
         from: process.env.PHONE_NUMBER, //NUMBER FROM TWILIO HERE
@@ -43,8 +39,7 @@ app.get('/send-text', (reg, res) => {
 
 
 //local host port 4000
-
-app.listen(3000, () => console.log("on port 4000"))
+app.listen(4000, () => console.log("on port 4000"))
 
 
 
