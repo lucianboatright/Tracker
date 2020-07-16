da2437a5f829334909cf0fd5bed5edb86fe2ae31
Lucian Boatright <lucian.boatright@gmail.com>
2020-07-16 17:54:37 +0100

    save state pre switch back to old

--- server/index.js
+++ server/index.js
@@ -1,50 +1,99 @@
-require('dotenv').config();
-console.log(process.env);
-// dependencies
-// yarn add express cors wtilio
-
-const express = require('express')
-const cors = require('cors')
-const twilio = require('twilio')
-
-// twilio rewuierments texting api
-
-const accountSid = process.env.ACCOUNT_SID // TWILIO SID NUM 
-const authToken = process.env.AUTH_TOKEN// TWILIO AUTH TOKEN
-const clinet = new twilio(accountSid, authToken)
-
-
-const app = express();  //alias
-
-
-app.use(cors());  //blocks browser from resticting data
-
-
-//welcome page server
-
-
-app.get('/', (reg, res) => {
-    res.send("WElcome from the server");
+import express from 'express';
+import path from 'path';
+import keys from './twiliokeys';
+import bodyParser from 'body-parser';
+const app = express();
+app.use('/', express.static('public'));
+app.post('/sendsms', bodyParser.json(), (req, res) => {
+  var client = require('twilio')(keys.sid, keys.token);
+  client.sendMessage({
+    to: req.body.data,
+    from: '+12223334444',
+    body: 'word to your mother.'
+  }, function (err, responseData) {
+    if (!err) {
+      res.json({"From": responseData.from, "Body": responseData.body});
+    }
+  })
 })
-
-// twilio text
-
-app.get('/send-text', (reg, res) => {
-    // GET variables via query string
-    const { recipient, textmessage } = reg.query
+app.listen(process.env.PORT || 3000);
 
 
-    clinet.messages.create({
-        body: textmessage,
-        to: recipient,
-        from: process.env.PHONE_NUMBER, //NUMBER FROM TWILIO HERE
-    }).then((message) => console.log(message.body))
-})
+
+// // import bodyParser from 'body-parser';
+// // import express from 'express';
+
+// require('dotenv').config();
+// console.log(process.env);
+// // dependencies
+// // yarn add express cors wtilio
+
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const pino = require('pino')
 
 
-//local host port 4000
 
-app.listen(4000, () => console.log("on port 4000"))
+// const cors = require('cors')
+// const twilio = require('twilio')
+
+// // twilio rewuierments texting api
+
+// const accountSid = process.env.ACCOUNT_SID // TWILIO SID NUM 
+// const authToken = process.env.AUTH_TOKEN// TWILIO AUTH TOKEN
+// const client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN)
+
+
+// const app = express();  //alias
+
+// // added from twilio site
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(pino);
+
+// app.post('/api/messages', (req, res) => {
+//     res.header('Content-Type', 'application/json');
+//     client.messages
+//       .create({
+//         from: process.env.TWILIO_PHONE_NUMBER,
+//         to: req.body.to,
+//         body: req.body.body
+//       })
+//       .then(() => {
+//         res.send(JSON.stringify({ success: true }));
+//       })
+//       .catch(err => {
+//         console.log(err);
+//         res.send(JSON.stringify({ success: false }));
+//       });
+//   });
+
+
+// // app.use(cors());  //blocks browser from resticting data
+
+
+// // //welcome page serve
+// // app.get('/', (reg, res) => {
+// //     res.send("WElcome from the server");
+// // })
+
+// // // twilio text
+// // app.post('/send-text', (reg, res) => {
+// //     // GET variables via query string
+// //     const { recipient, textmessage } = reg.query
+
+
+// //     clinet.messages.create({
+// //         body: textmessage,
+// //         to: recipient,
+// //         from: process.env.PHONE_NUMBER, //NUMBER FROM TWILIO HERE
+// //     }).then((message) => console.log(message.body))
+// // })
+
+
+// //local host port 4000
+
+// app.listen(4000, () => console.log("on port 4000"))
 
 
 
--- src/Components/TextButton.js
+++ src/Components/TextButton.js
@@ -4,55 +4,133 @@ import React, { Component } from 'react';
 
 
 class textButton extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          message: {
+            to: '',
+            body: ''
+          },
+          submitting: false,
+          error: false
+        };
+        this.onHandleChange = this.onHandleChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+      }
 
-    state = {
-        text: {
-            recipient: "",
-            textmessage: ""
-        }
-    }
+      onHandleChange(event) {
+        const name = event.target.getAttribute('name');
+        this.setState({
+          message: { ...this.state.message, [name]: event.target.value }
+        });
+      }
 
-    sendText = () => {
-        const { text } = this.state
-        //pass veriables within the query string
-        fetch(`http://localhost:4000/send-text?recipient=${text.recipient}&testmessage=${text.textmessage}`,console.log(text.recipient, text.textmessage))
-        .catch(err => console.log(err))
-    }
+      onSubmit(event) {
+        event.preventDefault();
+        this.setState({ submitting: true });
+        fetch('/sendsms', {
+            method: 'POST',
+            headers: {
+              Accept: 'application/JSON',
+              'Content-Type': 'application/JSON'
+            },
+            body: JSON.stringify({"data": this.state.data})
+          })
+          .then(res => res.json())
+          .then(data => {
+            if (data.success) {
+              this.setState({
+                error: false,
+                submitting: false,
+                message: {
+                  to: '',
+                  body: ''
+                }
+              });
+            } else {
+              this.setState({
+                error: true,
+                submitting: false
+              });
+            }
+          });
+          console.log("onSubmit")
+      } 
+    // state = {
+    //     text: {
+    //         recipient: "",
+    //         textmessage: ""
+    //     }
+    // }
+
+    // sendText = () => {
+    //     const { text } = this.state
+    //     //pass veriables within the query string
+    //     fetch(`http://localhost:4000/send-text?recipient=${text.recipient}&testmessage=${text.textmessage}`,console.log(text.recipient, text.textmessage))
+    //     .catch(err => console.log(err))
+    // }
 
     render() {
 
-        const { text } = this.state;
+        // const { text } = this.state;
 
-        const spacer = {
-            margi: 8
-        }
+        // const spacer = {
+        //     margi: 8
+        // }
 
-        const textArea = {
-            borderRadius: 4
-        }
+        // const textArea = {
+        //     borderRadius: 4
+        // }
 
 
         return (
-            <div className="textButtonBody">
-                <header className="buttonHeader">
-                    <h1 className="buttonTitle">Welcome</h1>
-                </header>
-                <div sytle={{ margin: 10 }}>
-                    <h2>Send text</h2>
-                    <label>Your Phone Number</label>
-                    <br />
-                    <input value={text.recipient}
-                            onChange={e => this.setState({ text: {...text, recipient: e.target.value} })} />
-                    <div style={spacer} />
-                    <label>Message</label>
-                    <br />
-                    <textarea rows={3} value={text.textmessage} style={textArea}
-                        onChange={e => this.setState({ text: {...text, textmessage: e.target.value} })} />
-                    <div style={spacer} />
-                    <button onClick={this.sendText}>Send Text</button>
-                
-                </div>
+            <form
+            onSubmit={this.onSubmit}
+            className={this.state.error ? 'error sms-form' : 'sms-form'}
+            >
+            <div>
+              <label htmlFor="to">To:</label>
+              <input
+                type="tel"
+                name="to"
+                id="to"
+                value={this.state.message.to}
+                onChange={this.onHandleChange}
+              />
             </div>
+            <div>
+              <label htmlFor="body">Body:</label>
+              <textarea
+                name="body"
+                id="body"
+                value={this.state.message.body}
+                onChange={this.onHandleChange}
+              />
+            </div>
+            {/* <button type="submit" disabled={this.state.submitting}>Send message</button> */}
+            <button onClick={this.sendSms.bind(this)}>Send SMS</button>
+          </form>
+
+            // <div className="textButtonBody">
+            //     <header className="buttonHeader">
+            //         <h1 className="buttonTitle">Welcome</h1>
+            //     </header>
+            //     <div sytle={{ margin: 10 }}>
+            //         <h2>Send text</h2>
+            //         <label>Your Phone Number</label>
+            //         <br />
+            //         <input value={text.recipient}
+            //                 onChange={e => this.setState({ text: {...text, recipient: e.target.value} })} />
+            //         <div style={spacer} />
+            //         <label>Message</label>
+            //         <br />
+            //         <textarea rows={3} value={text.textmessage} style={textArea}
+            //             onChange={e => this.setState({ text: {...text, textmessage: e.target.value} })} />
+            //         <div style={spacer} />
+            //         <button onClick={this.sendText}>Send Text</button>
+                
+            //     </div>
+            // </div>
         )
     }
 
