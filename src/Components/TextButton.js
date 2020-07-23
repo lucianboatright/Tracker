93c3f24ea68ba6346f49f7eaa01f484536cb57fe
Lucian Boatright <lucian.boatright@gmail.com>
2020-07-23 21:10:45 +0100

    form change on button component

--- src/Components/TextButton.js
+++ src/Components/TextButton.js
@@ -38,13 +38,13 @@ class textButton extends Component {
                     <h1 className="buttonTitle">Welcome</h1>
                 </header>
                 <div sytle={{ margin: 10 }}>
-                    <h2>Send text</h2>
+                    <h2>Send text form</h2>
                     <label>Enter Your Phone Number</label>
                     <br />
                     <input value={text.recipient}
                             onChange={e => this.setState({ text: {...text, recipient: e.target.value} })} />
                     <div style={spacer} />
-                    <label>Message to send</label>
+                    <label>Enter Message Below and Submit</label>
                     <br />
                     <textarea rows={3} value={text.textmessage} style={textArea}
                         onChange={e => this.setState({ text: {...text, textmessage: e.target.value} })} />
